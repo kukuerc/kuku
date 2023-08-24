9d1057b56270d8e8a873364afb2410d9fa78e0ed
kukuerc <142877038+kukuerc@users.noreply.github.com>
2023-08-24 15:38:19 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -45,7 +45,10 @@ function getTranslations(){
             "me3": "KuKu is be showcased on the large screen in Times Square, New York.",
             "me4": "The most united community",
             "me5": "Contract: 0x27206f5a9afd0c51da95f20972885545d3b33647",
-            "meme": "The MEME created by the KUKU community"
+            "meme": "The MEME created by the KUKU community",
+            "ecology": "kuku Ecology",
+            "ecologytext": "A new ecological gameplay composed of $KuKu $QoQo KuKu NFT",
+            "soon": "Coming Soon"
         },
         'zh': {
             "welcome": "嗨，我是KuKu",
@@ -54,7 +57,10 @@ function getTranslations(){
             "me3": "KuKu在紐約時代廣場的大屏幕上展示",
             "me4": "最團結的社群",
             "me5": "合約: 0x27206f5a9afd0c51da95f20972885545d3b33647",
-            "meme": "由KUKU社群創建的MEME"
+            "meme": "由KUKU社群創建的MEME",
+            "ecology": "kuku Ecology",
+            "ecologytext": "由$KuKu $QoQo KuKu NFT組成的全新生態玩法",
+            "soon": "不久後將會推出 Coming Soon"
         },
         'jp': {
             "welcome": "こんにちは、私はKuKuです",
@@ -63,7 +69,10 @@ function getTranslations(){
             "me3": "KuKuはニューヨークのタイムズスクエアの大画面で展示されています",
             "me4": "最も団結したコミュニティ",
             "me5": "契約: 0x27206f5a9afd0c51da95f20972885545d3b33647",
-            "meme": "KUKUコミュニティによって作成されたMEME"
+            "meme": "KUKUコミュニティによって作成されたMEME",
+            "ecology": "kuku Ecology",
+            "ecologytext": "$KuKu $QoQo KuKu NFTから成る新しいエコロジーゲーム",
+            "soon": "近日公開"
         },
         'kr': {
             "welcome": "안녕하세요, 나는 KuKu입니다",
@@ -72,7 +81,10 @@ function getTranslations(){
             "me3": "KuKu는 뉴욕 타임스 스퀘어의 큰 화면에 전시되어 있습니다",
             "me4": "가장 단결된 커뮤니티",
             "me5": "계약: 0x27206f5a9afd0c51da95f20972885545d3b33647",
-            "meme": "KUKU 커뮤니티가 만든 MEME"
+            "meme": "KUKU 커뮤니티가 만든 MEME",
+            "ecology": "kuku Ecology",
+            "ecologytext": "$KuKu $QoQo KuKu NFT로 구성된 새로운 생태 게임",
+            "soon": "곧 출시될 예정"
         },
         'cn': {
             "welcome": "嗨，我是KuKu",
@@ -81,7 +93,10 @@ function getTranslations(){
             "me3": "KuKu在纽约时代广场的大屏幕上展示",
             "me4": "最团结的社群",
             "me5": "合约: 0x27206f5a9afd0c51da95f20972885545d3b33647",
-            "meme": "由KUKU社区创建的MEME"
+            "meme": "由KUKU社区创建的MEME",
+            "ecology": "kuku Ecology",
+            "ecologytext": "由$KuKu $QoQo KuKu NFT组成的全新生态玩法",
+            "soon": "不久后将会推出 Coming Soon"
         }
     };
 
